7aa8b8eed92df87aa15d412bdd379fe237a5996a
cmllucmajor <dev@cmllucmajor.com>
2021-02-28 21:53:45 +0100

    Updates

---
+++ precache-manifest.3bc577a4d3a7d5b8cb223426d6feae00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0198fca9b3b11de397799835ee7433c",
+    "url": "/cmllucmajor.com/index.html"
+  },
+  {
+    "revision": "31c6bb4b12a5d3c25e78",
+    "url": "/cmllucmajor.com/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "869732eb40d98085f834",
+    "url": "/cmllucmajor.com/static/js/2.1c76f135.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cmllucmajor.com/static/js/2.1c76f135.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31c6bb4b12a5d3c25e78",
+    "url": "/cmllucmajor.com/static/js/main.7cfae6f5.chunk.js"
+  },
+  {
+    "revision": "d98381df38d6778d208c",
+    "url": "/cmllucmajor.com/static/js/runtime-main.e9219bde.js"
+  }
+]);
